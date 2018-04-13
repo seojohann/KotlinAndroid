92a3122f07afc01104bcff64e36bca8510728ee6
seojohann <seojohann@gmail.com>
2018-04-13 00:06:14 -0700

    initial commit

---
+++ app/src/main/java/com/jsbomb/apps/kotlinnoifcalc/NoIfCalculator.kt
@@ -0,0 +1,12 @@
+package com.jsbomb.apps.kotlinnoifcalc
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class NoIfCalculator : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_no_if_calculator)
+    }
+}
